2789b3bd218003c17e3920e9c49ebcabeaaffac8
Noel <noel.a.plisko@gmail.com>
2021-09-23 17:36:39 +0200

    django authentication

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,10 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
+
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -54,9 +58,27 @@ def login(request):
         user = auth.authenticate(email = email, password = password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, "You are now logged in!")
-            return redirect("dashboard")
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split("=") for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)                                                                                                                                                                                                                                                         
+            except:
+                return redirect('dashboard')
         else:
             messages.ERROR(request, "Invalid info")
             return redirect("login")
--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -15,6 +16,7 @@ def _cart_id(request):
 	return cart
 
 def add_cart(request, product_id):
+	current_user = request.user
 	product = Product.objects.get(id = product_id)
 	try:
 		cart = Cart.objects.get(cart_id = _cart_id(request))
@@ -22,7 +24,7 @@ def add_cart(request, product_id):
 		cart = Cart.objects.create(
 			cart_id = _cart_id(request)
 			)
-	cart.save()
+	cart.save()	
 
 	try:
 		cart_item = CartItem.objects.get(product = product, cart = cart)
@@ -33,26 +35,33 @@ def add_cart(request, product_id):
 			product = product,
 			quantity = 1,
 			cart = cart,
-		) 
-		cart_item.save()
-	return HttpResponse(cart_item.product)
-	exit()
+			) 
+		cart_item.save()			
 	return redirect('cart')
 
 def remove_cart(request, product_id):
-	cart = Cart.objects.get(cart_id = _cart_id(request))
 	product = get_object_or_404(Product, id = product_id)
-	cart_item = CartItem.objects.get(product = product, cart = cart)
-	if cart_item.quantity > 1:
-		cart_item.quantity -=1
-		cart_item.quantity.save()
-	else:
-		cart_item.delete()
+	try:
+		if request.user.is_authenticated:
+			cart_item = CartItem.objects.get(product = product, user = request.user, id = cart_item_id)	
+		else:
+			cart = Cart.objects.get(cart_id = _cart_id(request))
+			cart_item = CartItem.objects.get(product = product, cart = cart, id = cart_item_id)	
+		if cart_item.quantity > 1:
+			cart_item.quantity -=1
+			cart_item.quantity.save()
+		else:
+			cart_item.delete()
+	except:
+		pass
 	return redirect('cart')
 
 def remove_cart_item(request, product_id):
-	cart = Cart.objects.get(cart_id = _cart_id(request))
 	product = get_object_or_404(Product, id = product_id)
+	if request.user.is_authenticated:
+		cart_item = CartItem.objects.get(product = product, user = request.user, id = cart_item_id)	
+	else:
+		cart = Cart.objects.get(cart_id = _cart_id(request))
 	cart_item = CartItem.objects.get(product = product, cart = cart)
 	cart_item.delete()
 	return redirect('cart')
@@ -62,8 +71,39 @@ def cart(request, total = 0, quantity = 0, cart_items = None):
 	try:
 		tax = 0
 		grand_total = 0
-		cart = Cart.objects.get(cart_id = _cart_id(request))
-		cart_items = CartItem.objects.filter(cart = cart, is_active = True)
+		if request.user.is_authenticated:
+			cart_items = CartItem.objects.filter(user = request.user, is_active = True)
+		else:
+			cart = Cart.objects.get(cart_id = _cart_id(request))
+			cart_items = CartItem.objects.filter(cart = cart, is_active = True)
+		for cart_item in cart_items:
+			total += (cart_item.product.price * cart_item.quantity)
+			quantity += cart_item.quantity
+		tax = (2 * total)/100
+		grand_total = total + tax
+	except ObjectDoesNotExist:
+		pass
+
+	context = {
+		'total':total,
+		'quantity':quantity,
+		'cart_items':cart_items,
+		'tax' : tax,
+		'grand_total' : grand_total,
+	}
+
+	return render(request, 'store/cart.html',context)
+
+@login_required(login_url='login')
+def checkout(request, total = 0, quantity = 0, cart_items = None):
+	try:
+		tax = 0
+		grand_total = 0
+		if request.user.is_authenticated:
+			cart_items = CartItem.objects.filter(user = request.user, is_active = True)
+		else:
+			cart = Cart.objects.get(cart_id = _cart_id(request))
+			cart_items = CartItem.objects.filter(cart = cart, is_active = True)
 		for cart_item in cart_items:
 			total += (cart_item.product.price * cart_item.quantity)
 			quantity += cart_item.quantity
@@ -80,5 +120,5 @@ def cart(request, total = 0, quantity = 0, cart_items = None):
 	'grand_total' : grand_total,
 	}
 
-	return render(request, 'store/cart.html',context)
+	return render(request, 'store/checkout.html', context)
 
